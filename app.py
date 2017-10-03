d7f90ad4642291ba6cd601b4c1caa412a9f75534
bot-rpa <32352458+bot-rpa@users.noreply.github.com>
2017-10-03 14:03:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,84 @@
 
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+import smtplib
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    keyword = parameters.get("keyword")
+    
+    user="ai.bot.chat@gmail.com"
+    recipient="ai.bot.chat@gmail.com"
+    subject = "Regression LPS - " + keyword
+    body = "Please test " + keyword
+    gmail_user = "ai.bot.chat"
+    gmail_pwd = "Chatbot@2017"
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server_ssl.ehlo()
+        server_ssl.login(gmail_user, gmail_pwd)  
+        server_ssl.sendmail(FROM, TO, message)
+        server_ssl.close()
+        print('successfully sent the mail')
+    except Exception as ex:
+        print(ex)
+        
+    res = makeWebhookResult(keyword)
+    return res
+
+def makeWebhookResult(keyword):
+    output_speech = "Test Scenario '" + keyword + "' has been initiated"
+    return {
+        "speech": output_speech,
+        "displayText": output_speech,
+        "source": "apiai-rpabot-webhook-sample"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
